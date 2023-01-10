556c4871c38fafa3153bc7fd05e19b55cdf054f2
HwonPark <ioio6085@naver.com>
2023-01-11 00:23:39 +0900

    .

--- src/main/java/project/flower/repository/AdminRepository.java
+++ src/main/java/project/flower/repository/AdminRepository.java
@@ -1,4 +1,11 @@
 package project.flower.repository;
 
-public interface AdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import project.flower.domain.Admin;
+
+import java.util.Optional;
+
+public interface AdminRepository extends JpaRepository<Admin, Long> { //JpaRepository=> findById, findAll사용가능
+    //Admin save(Admin admin);
+    Optional<Admin> findById(Long id);
 }
