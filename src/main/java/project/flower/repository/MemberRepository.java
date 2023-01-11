896a2b6d506c707237ecbb9b0ff937770387175e
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-11 17:42:47 +0900

    fix(memberRepository): 로그인 관련 기능 오류로 잠시 주석처리

--- src/main/java/project/flower/repository/MemberRepository.java
+++ src/main/java/project/flower/repository/MemberRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.flower.domain.member.Member;
 
+import java.util.Optional;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    // 회원 가입 후 로그인시 사용
+    //Optional<Member> findByMemberId (String memberId);
 }
