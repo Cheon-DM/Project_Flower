743a707bca08c01fc6cfbd493e3c70a968bc0a92
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-14 01:37:14 +0900

    feat(cart): 장바구니 관련 DB 생성

--- src/main/java/project/flower/domain/cart/Cart.java
+++ src/main/java/project/flower/domain/cart/Cart.java
@@ -1,7 +1,31 @@
 package project.flower.domain.cart;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.flower.domain.member.Member;
+
+import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
 public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
+    // FK
+    @OneToMany (mappedBy = "cartId")
+    private List<CartItem> cartIdList;
+
 }
