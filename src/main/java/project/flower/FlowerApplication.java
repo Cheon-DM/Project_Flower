16ba567ccb28fea5a0fb676f97dcd740f5abd717
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-19 00:28:52 +0900

    feat(main): main page 경로 컨트롤러 임시 추가

--- src/main/java/project/flower/FlowerApplication.java
+++ src/main/java/project/flower/FlowerApplication.java
@@ -2,6 +2,7 @@ package project.flower;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class FlowerApplication {
@@ -10,4 +11,8 @@ public class FlowerApplication {
 		SpringApplication.run(FlowerApplication.class, args);
 	}
 
+	@RequestMapping("/")
+	public String homePage() {
+		return "index.html";
+	}
 }
