e7d6874987ba3ace6f9fd32b9d57b4f0db25c43b
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-18 18:04:48 +0900

    feat(securityConfig): 로그인 설정 추기

--- src/main/java/project/flower/SecurityConfig.java
+++ src/main/java/project/flower/SecurityConfig.java
@@ -3,10 +3,10 @@ package project.flower;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import project.flower.service.MemberDetailService;
@@ -26,17 +26,27 @@ public class SecurityConfig{
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable();
+
         // 해당 경로 접근 허용
-        http.authorizeHttpRequests().requestMatchers("/css/**", "/js/**", "/img/**").permitAll()
+        http.authorizeHttpRequests().requestMatchers("/", "/basic/cart", "/index.html", "/basic/loginProc", "/basic/signupProc", "/basic/signup/**", "/basic/login/**", "/css/**", "/favicon.ico", "/**/favicon.ico").permitAll()
                 .anyRequest().authenticated();
+
         // 로그인 페이지 지정
-        http.formLogin().loginPage("/member/login")
-                .loginProcessingUrl("/")
-                .defaultSuccessUrl("/");
+        http.formLogin().loginPage("/basic/login/customer")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .loginProcessingUrl("/basic/loginProc")
+                .defaultSuccessUrl("/").permitAll();
+
         // 로그아웃 페이지
         http.logout().logoutSuccessUrl("/").invalidateHttpSession(true);
 
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception {
+        return auth.getAuthenticationManager();
     }
 
 }
