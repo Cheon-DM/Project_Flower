348d47feb888353e66d248749178f1571f6796a1
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-18 18:04:03 +0900

    refactor(memberRepository): 로그인 시 email로 회원 조회 인터페이스 구현

--- src/main/java/project/flower/repository/MemberRepository.java
+++ src/main/java/project/flower/repository/MemberRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     // 회원 가입 후 로그인시 사용
-    Optional<Member> findByMemberEmail (String memberId);
+    Optional<Member> findByEmail (String memberEmail);
 }
