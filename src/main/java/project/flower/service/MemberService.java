a5d8b5563979e1706b4c04c7d5a850c0f2b2f7b4
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-20 23:14:10 +0900

    feat(memberService): 회원가입 시 장바구니 데이터 생성 추가

--- src/main/java/project/flower/service/MemberService.java
+++ src/main/java/project/flower/service/MemberService.java
@@ -1,12 +1,14 @@
 package project.flower.service;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import project.flower.domain.cart.Cart;
+import project.flower.domain.member.Member;
 import project.flower.domain.member.MemberForm;
+import project.flower.repository.CartRepository;
 import project.flower.repository.MemberRepository;
 
 @Slf4j
@@ -15,11 +17,16 @@ import project.flower.repository.MemberRepository;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
     private final BCryptPasswordEncoder encoder;
 
     @Transactional
-    public Long join(MemberForm form){
-        form.setPassword(encoder.encode(form.getPassword()));
-        return memberRepository.save(form.toEntity()).getId();
+    public void join(MemberForm form){
+        Cart cart = Cart.builder().build();
+        Member member = form.toEntity();
+        member.setPassword(encoder.encode(form.getPassword()));
+        cart.setMember(member);
+        member.setCart(cart);
+        memberRepository.save(member).getId();
     }
 }
