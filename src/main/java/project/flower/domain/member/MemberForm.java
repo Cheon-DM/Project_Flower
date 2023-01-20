869ee30251a5fc8dfa905334150d3bf377f006f8
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-20 17:38:05 +0900

    feat(memberForm): validation 추가

--- src/main/java/project/flower/domain/member/MemberForm.java
+++ src/main/java/project/flower/domain/member/MemberForm.java
@@ -1,9 +1,11 @@
 package project.flower.domain.member;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
@@ -11,10 +13,18 @@ import lombok.NoArgsConstructor;
 @Builder
 public class MemberForm {
 
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+    @NotBlank(message = "이메일은 필수 입력 값입니다.")
     private String email;
+    @NotBlank(message = "비밀번호는 필수 입력 값입니다.")
     private String password;
-    private int age;
+    @Range(min = 0, max = 200, message = "나이의 값이 올바르지 않습니다.")
+    @NotNull(message = "나이는 필수 입력 값입니다.")
+    private Integer age;
+    @NotBlank(message = "성별은 필수 입력 값입니다.")
     private String sex;
 
     // Form(DTO) -> Entity
