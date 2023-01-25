7262753a8b14b0491a00f9fca560ddf20d05fe08
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-26 00:59:24 +0900

    feat(memberDetails): getter 주입

--- src/main/java/project/flower/domain/member/MemberDetails.java
+++ src/main/java/project/flower/domain/member/MemberDetails.java
@@ -1,12 +1,14 @@
 package project.flower.domain.member;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+@Getter
 @AllArgsConstructor
 public class MemberDetails implements UserDetails {
 
