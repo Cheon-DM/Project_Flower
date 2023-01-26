672647934becab92615480d0deb232e52881414f
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-27 00:44:50 +0900

    feat(favorite): 즐겨찾기 구현

---
+++ src/main/java/project/flower/controller/FavoriteController.java
@@ -0,0 +1,31 @@
+package project.flower.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import project.flower.domain.member.MemberDetails;
+import project.flower.service.FavoriteService;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    @GetMapping("/add/{businessId}")
+    public String addBusiness(@PathVariable Long businessId,
+                              @AuthenticationPrincipal MemberDetails memberDetails, Model model) {
+        log.info("business id={}",businessId);
+        log.info("user={}",memberDetails.getMember());
+        model.addAttribute("user", memberDetails.getMember());
+        favoriteService.saveStore(businessId, memberDetails.getMember());
+        return "redirect:/";
+    }
+
+}
