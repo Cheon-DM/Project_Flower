53026c4c7e2a46c4168dda6e49d4771e15e53fe6
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-26 19:53:36 +0900

    fix(adminController): 회원가입 컨트롤러 수정

--- src/main/java/project/flower/controller/AdminController.java
+++ src/main/java/project/flower/controller/AdminController.java
@@ -1,11 +1,14 @@
 package project.flower.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
-import project.flower.domain.admin.AdminForm;
-import project.flower.service.AdminService;
+import project.flower.domain.member.MemberForm;
+import project.flower.service.MemberService;
 
 @Slf4j
 @Controller
@@ -13,33 +16,36 @@ import project.flower.service.AdminService;
 @RequiredArgsConstructor
 public class AdminController {
 
-    private final AdminService adminService;
+    private final MemberService memberService;
 
     @GetMapping("/signup/seller")
-    public String signUpSellerForm(){
+    public String signUpSellerForm(@ModelAttribute("form") MemberForm form){
 
         return "signup/seller";
     }
 
     @PostMapping("/signup/seller")
-    public  String signUpSeller(@ModelAttribute AdminForm form){
-        log.info("email = {}, password = {}, name = {}, bus_num ={}, bus_name={}, age={}", form.getName(),
-                form.getPassword(), form.getName(), form.getBusinessNum(), form.getBusinessName(),form.getAge());
-        adminService.createAdmin(form);
-        return "redirect:/";
+    public  String signUpSeller(@Valid @ModelAttribute("form") MemberForm form, BindingResult bindingResult){
+        log.info("name = {}, email = {}, password = {}, age = {}, sex = {}",
+                form.getName(), form.getEmail(), form.getPassword(), form.getAge(), form.getSex());
+
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+
+            // 회원가입 페이지로 다시 이동
+            return "signup/customer";
+        }
+
+        // 중복 체크
+        try {
+            memberService.validateDuplicateMember(form.toUserEntity());
+        } catch (Exception e) {
+            bindingResult.addError(new FieldError("form", "email", e.getMessage()));
+            return "signup/customer";
+        }
+
+        // 성공로직
+        memberService.joinAdmin(form);
+        return "redirect:/login";
     }
-
-    @GetMapping("/signin/seller")
-    public String signInSellerForm(@ModelAttribute AdminForm form){
-
-        return "signin/seller";
-    }
-
-    @PostMapping("/signin/seller")
-    public String signInSeller(@ModelAttribute AdminForm form){
-        log.info("email = {}, password = {}", form.getEmail(), form.getPassword());
-        adminService.signInAdmin(form.getEmail(), form.getPassword());
-        return "redirect:/";
-    }
-
 }
