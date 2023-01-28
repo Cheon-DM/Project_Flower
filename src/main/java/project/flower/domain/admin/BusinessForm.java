dd581e8d5090a241b09ccfda914eae5eefd2503e
HwonPark <ioio6085@naver.com>
2023-01-28 22:23:28 +0900

    feat(BusinessServics): register 구현

---
+++ src/main/java/project/flower/domain/admin/BusinessForm.java
@@ -0,0 +1,27 @@
+package project.flower.domain.admin;
+
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.flower.domain.member.Member;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BusinessForm {
+
+    private String businessName;
+    private int businessNum;
+
+    public Business toEntity(Member member){
+        return Business.builder()
+                .businessName(businessName)
+                .businessNum(businessNum)
+                .member(member)
+                .build();
+    }
+}
--- src/main/java/project/flower/service/BusinessService.java
+++ src/main/java/project/flower/service/BusinessService.java
@@ -3,6 +3,9 @@ package project.flower.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import project.flower.domain.admin.Business;
+import project.flower.domain.admin.BusinessForm;
+import project.flower.domain.member.Member;
 import project.flower.repository.BusinessRepository;
 
 @Slf4j
@@ -11,5 +14,13 @@ import project.flower.repository.BusinessRepository;
 public class BusinessService {
 
     private final BusinessRepository businessRepository;
+
+    public Long registerBusiness(BusinessForm form, Member member){
+
+        Business business = form.toEntity(member);
+        business.setMember(member);
+
+        return businessRepository.save(business).getId();
+    }
 
 }
