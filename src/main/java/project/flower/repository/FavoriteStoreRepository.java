ec21ae0bfe2841d72fb8fb17d21b4f18ba8a4675
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-01-31 00:13:41 +0900

    feat(favoriteStoreRepository): 해당 회원의 즐겨찾기 목록 출력 함수

--- src/main/java/project/flower/repository/FavoriteStoreRepository.java
+++ src/main/java/project/flower/repository/FavoriteStoreRepository.java
@@ -2,7 +2,12 @@
 package project.flower.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.flower.domain.favorite.Favorite;
 import project.flower.domain.favorite.FavoriteStore;
 
+import java.util.List;
+
 public interface FavoriteStoreRepository extends JpaRepository<FavoriteStore, Long> {
+
+    public List<FavoriteStore> findAllByFavorite(Favorite id);
 }
