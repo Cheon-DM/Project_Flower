d225d976bea5457cb6a170ea46fa34036c68a760
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2023-03-03 01:54:27 +0900

    style: 디자인 변경 및 사이드 바 변경

--- src/main/java/project/flower/controller/BusinessController.java
+++ src/main/java/project/flower/controller/BusinessController.java
@@ -64,11 +64,15 @@ public class BusinessController {
     }
 
     @GetMapping("/admin/businesses/{businessId}")
-    public String flowerList(@PathVariable long businessId, Model model){
+    public String flowerList(@AuthenticationPrincipal MemberDetails memberDetails,
+                             @PathVariable long businessId, Model model){
 
         Business business = businessService.findBusiness(businessId);
         List<FlowerBouquet> bouquetList = business.getBouquetList();
         List<FlowerSingle> singleList = business.getSingleList();
+        List<Business> businessList = memberDetails.getMember().getBusinessList();
+        model.addAttribute("businessList", businessList);
+
         model.addAttribute("bouquetList", bouquetList);
         model.addAttribute("singleList", singleList);
         model.addAttribute("business", business);
