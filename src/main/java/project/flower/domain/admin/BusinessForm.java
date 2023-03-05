6b66bd26eeb40d688d934b1d97696680858ea777
HwonPark <ioio6085@naver.com>
2023-03-05 17:14:45 +0900

    style: form valid 추가

--- src/main/java/project/flower/domain/admin/BusinessForm.java
+++ src/main/java/project/flower/domain/admin/BusinessForm.java
@@ -1,10 +1,14 @@
 package project.flower.domain.admin;
 
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.web.multipart.MultipartFile;
 import project.flower.domain.member.Member;
 
@@ -14,8 +18,12 @@ import project.flower.domain.member.Member;
 @Builder
 public class BusinessForm {
 
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-zA-Z0-9-_]{1,20}$", message = "업체명은 특수문자를 제외한 1~20자리여야 합니다.")
+    @NotBlank(message = "업체명은 필수 입력 값입니다.")
     private String businessName;
-    private int businessNum;
+    @Pattern(regexp = "^[0-9-_]{10}$", message = "10자리여야 합니다.")
+    @NotBlank(message = "사업자 번호은 필수 입력 값입니다.")
+    private String businessNum;
 
     private MultipartFile imgFile;
 
